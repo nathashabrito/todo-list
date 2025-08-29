f41b052682f90fee651ceccf5a6abffe3f9d31f5
nathashabrito <britonathasha@gmail.com>
2025-08-29 10:19:12 -0300

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,9 @@
 import { buildApp } from "./src/app";
 
+const app = buildApp();
+
 export default async function handler(req: any, res: any) {
-  const app = buildApp();
   await app.ready();
-  
-  // Remove o prefixo /api da URL para o Fastify
-  const url = req.url.replace('/api', '') || '/';
-  
-  const response = await app.inject({
-    method: req.method,
-    url: url,
-    headers: req.headers,
-    payload: req.body
-  });
-  
-  res.status(response.statusCode);
-  Object.keys(response.headers).forEach(key => {
-    res.setHeader(key, response.headers[key]);
-  });
-  
-  res.send(response.body);
+  app.server.emit('request', req, res);
 }
 
