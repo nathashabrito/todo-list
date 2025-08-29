1263f2b751dcd6cdf62fb5367c24a656e673c06a
nathashabrito <britonathasha@gmail.com>
2025-08-29 10:11:16 -0300

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,25 @@
 import { buildApp } from "./src/app";
 
-const app = buildApp();
+let app: any;
 
 export default async function handler(req: any, res: any) {
-  await app.ready();
-  app.server.emit("request", req, res);
+  if (!app) {
+    app = buildApp();
+    await app.ready();
+  }
+  
+  const response = await app.inject({
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+    payload: req.body
+  });
+  
+  res.status(response.statusCode);
+  Object.keys(response.headers).forEach(key => {
+    res.setHeader(key, response.headers[key]);
+  });
+  
+  res.send(response.body);
 }
 
